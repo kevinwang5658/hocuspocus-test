0684ae27ba96f168fcc7f88addc9845b7981cf23
kevinwang <kevinwang5658@gmail.com>
2025-06-21 10:56:47 -0400

    Added authentication and fixed store method to update instead of insert

--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,82 @@
 import { Server } from "@hocuspocus/server";
-import {onConnectPayload, onStoreDocumentPayload} from "@hocuspocus/server/dist/packages/server/src/types";
+import {
+  onAuthenticatePayload,
+  onConnectPayload,
+  onStoreDocumentPayload
+} from "@hocuspocus/server/dist/packages/server/src/types";
 import {createClient} from "@supabase/supabase-js";
 import 'dotenv/config';
-import {Database as SupabaseTypes } from "./database.types";
 import { Database } from "@hocuspocus/extension-database";
+import * as jose from 'jose';
 
 // Supabase
-const supabase = createClient<SupabaseTypes>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-)
+export const createSupabaseClient = (accessToken: string) => {
+  return createClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      global: {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    }
+  )
+}
+
 
 // Configure the server …
 const server = new Server({
-  port: 3000,
+  port: 3001,
+  async onAuthenticate(data: onAuthenticatePayload): Promise<any> {
+    const { token } = data;
+
+    try {
+      const userJwt = jose.decodeJwt(token);
+      const {
+        sub: userId,
+        email: email,
+      } = userJwt;
+
+      // You can set contextual data to use it in other hooks
+      return {
+        userId,
+        email,
+        token
+      };
+    } catch (error) {
+
+    }
+  },
+
   async onConnect(data: onConnectPayload): Promise<void> {
     console.log(`New connection ${data.socketId}`);
   },
 
   extensions: [
     new Database({
-      fetch: async ({ documentName }) => {
+      fetch: async ({ documentName, context }) => {
+        const supabase = createSupabaseClient(context.token);
+
         const row = await supabase.from('documents')
           .select('*')
+          .eq('id', documentName)
+          .eq('user_id', context.userId)
 
-        return row.data.length > 0
+        return (row.data.length > 0 && row.data[0].blob)
           ? Buffer.from(row.data[0].blob.slice(2), 'hex')
           : null;
       },
-      store: async ({ documentName, state }) => {
-        console.log('store');
+      store: async ({ documentName, state, context, document }) => {
+        console.log(`store ${documentName}`);
 
+        const supabase = createSupabaseClient(context.token);
         await supabase.from('documents')
-          .insert({
-            name: documentName,
-            blob: '\\x' + state.toString('hex')
+          .update({
+            blob: '\\x' + state.toString('hex'),
           })
+          .eq('id', documentName)
+          .eq('user_id', context.userId)
       }
     })
   ]
